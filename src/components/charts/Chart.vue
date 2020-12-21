be08ceff3775ef8b768916586a960111dce730fd
44232170+EragonChanel@users.noreply.github.com <44232170+EragonChanel@users.noreply.github.com>
2020-12-21 13:22:06 +0000

    first draft of vue charts

---
+++ src/components/charts/Chart.vue
@@ -0,0 +1,61 @@
+<script>
+import { Line } from 'vue-chartjs'
+
+export default {
+  extends: Line,
+  props: {
+    chartData: {
+      type: Array,
+      default: null
+    },
+    chartLabels: {
+      type: Array,
+      required: true
+    }
+  },
+  data () {
+    return {
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            },
+            gridLines: {
+              display: true
+            }
+          }],
+          xAxes: [{
+            gridLines: {
+              display: false
+            }
+          }]
+        },
+        legend: {
+          display: false
+        },
+        tooltips: {
+          intersect: false
+        },
+        responsive: true,
+        maintainAspectRatio: false
+      }
+    }
+  },
+  mounted () {
+    this.renderChart({
+      labels: this.chartLabels,
+      datasets: [
+        {
+          label: 'STX to BTC',
+          borderColor: '#249EBF',
+          borderWidth: 3,
+          pointRadius: 0,
+          backgroundColor: 'transparent',
+          data: this.chartData
+        }
+      ]
+    }, this.options)
+  }
+}
+</script>
